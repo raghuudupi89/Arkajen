8984c868519fda572418693ebbea7b1bb2d20cd4
Raghav <raghuudupi@gmail.com>
2014-08-28 11:28:58 +0530

    ArkajenCode

---
+++ arkajen/index.php
@@ -0,0 +1,117 @@
+<?php 
+    require("config.php"); 
+    $submitted_username = ''; 
+    if(!empty($_POST)){ 
+        $query = " 
+            SELECT 
+                id, 
+                username, 
+                password, 
+                salt, 
+                email 
+            FROM users 
+            WHERE 
+                username = :username 
+        "; 
+        $query_params = array( 
+            ':username' => $_POST['username'] 
+        ); 
+         
+        try{ 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+        $login_ok = false; 
+        $row = $stmt->fetch(); 
+        if($row){ 
+            $check_password = hash('sha256', $_POST['password'] . $row['salt']); 
+            for($round = 0; $round < 65536; $round++){
+                $check_password = hash('sha256', $check_password . $row['salt']);
+            } 
+            if($check_password === $row['password']){
+                $login_ok = true;
+            } 
+        } 
+
+        if($login_ok){ 
+            unset($row['salt']); 
+            unset($row['password']); 
+            $_SESSION['user'] = $row;  
+            header("Location: snippets/Menu.html"); 
+            die("Redirecting to: secret.php"); 
+        } 
+        else{ 
+            print("Login Failed."); 
+            $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8'); 
+        } 
+    } 
+?> 
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<link rel="icon" type="image/png" href="img/logo.png">
+    <title>Arkajen</title>
+    <meta name="description" content="Vzure Internal">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="assets/bootstrap.min.js"></script>
+    <link href="assets/bootstrap.min.css" rel="stylesheet" media="screen">
+    <style type="text/css">
+        body { background: url(assets/bglight.png); }
+        .hero-unit { background-color: #fff; }
+        .center { display: block; margin: 0 auto; }
+    </style>
+</head>
+
+<body>
+
+<div class="navbar navbar-fixed-top navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+	<div class="logo">
+				<img src="img/logo.png" onclick="www.google.com">
+			</div>
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a></br>
+	  <a class="brand">Welcome : Arkajen Internal Application</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav pull-right">
+          <li><a href="register.php">Register</a></li>
+          <li class="divider-vertical"></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown">Log In <strong class="caret"></strong></a>
+            <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+                <form action="index.php" method="post"> 
+                    Username:<br /> 
+                    <input type="text" name="username" value="<?php echo $submitted_username; ?>" /> 
+                    <br /><br /> 
+                    Password:<br /> 
+                    <input type="password" name="password" value="" /> 
+                    <br /><br /> 
+                    <input type="submit" class="btn btn-info" value="Login" /> 
+                </form> 
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="container hero-unit">
+    <ul>
+     <li>Register or use the default credentials:<br />
+            <strong>username:</strong> superadmin<br />
+            <strong>password:</strong> superadmin<br /></li>
+    </ul>
+</div>
+</div>
+
+
+
+</body>
+</html>
